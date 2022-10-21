65f399fd995722e59ef1bae81ad1082199e0c7b8
kakukalus <66096781+kakukalus@users.noreply.github.com>
2022-10-21 19:15:16 +0700

    Create pertemuan_3.php

---
+++ pertemuan_3/pertemuan_3.php
@@ -0,0 +1,74 @@
+<?php
+
+# membuat class Animal
+class Animal
+{
+  # property animals
+
+  # method constructor - mengisi data awal
+  # parameter: data hewan (array)
+  var $data = array(
+    'Kerbau',
+    'Sapi',
+  );
+
+  public function __construct($data)
+  {
+    $this->data;
+  }
+
+  # method index - menampilkan data animals
+  public function index()
+  { 
+    foreach ($this->data as $key => $value) {
+      echo $value."<br>";
+    }
+    # gunakan foreach untuk menampilkan data animals (array)
+  }
+
+  # method store - menambahkan hewan baru
+  # parameter: hewan baru
+  public function store($data)
+  {
+    array_push($this->data,$data);
+    # gunakan method array_push untuk menambahkan data baru
+  }
+
+  # method update - mengupdate hewan
+  # parameter: index dan hewan baru
+  public function update($index, $data)
+  {
+    $this->data[$index] = $data;
+  }
+
+  # method delete - menghapus hewan
+  # parameter: index
+  public function destroy($index)
+  {
+    unset($this->data[$index]);
+    # gunakan method unset atau array_splice untuk menghapus data array
+  }
+}
+
+# membuat object
+# kirimkan data hewan (array) ke constructor
+$animal = new Animal([]);
+
+echo "Index - Menampilkan seluruh hewan <br>";
+$animal->index();
+echo "<br>";
+
+echo "Store - Menambahkan hewan baru <br>";
+$animal->store('burung');
+$animal->index();
+echo "<br>";
+
+echo "Update - Mengupdate hewan <br>";
+$animal->update(0, 'Kucing Anggora');
+$animal->index();
+echo "<br>";
+
+echo "Destroy - Menghapus hewan <br>";
+$animal->destroy(1);
+$animal->index();
+echo "<br>";
